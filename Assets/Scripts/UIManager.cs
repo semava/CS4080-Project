b25fd2f91d967b120de0f96b2e12373d172fafbd
dev0nati0n <112506177+dev0nati0n@users.noreply.github.com>
2022-12-04 02:57:44 -0800

    Added Main Menu with WIP managers

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+
+    // create singleton to ensure only one of these exists at one time
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false; // allows quit functionality in editor mode
+    }
+}
